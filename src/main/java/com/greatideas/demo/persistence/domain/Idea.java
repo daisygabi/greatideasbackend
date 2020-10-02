3ff07fd1ca0516046faf903a48943dcfb29ac6c0
Gabi <gabrielaradu.developer@gmail.com>
2020-10-02 17:50:57 +0300

    Describe domain and dto for ideas pool table.

---
+++ src/main/java/com/greatideas/demo/persistence/domain/Idea.java
@@ -0,0 +1,40 @@
+package com.greatideas.demo.persistence.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "ideas_pool")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Idea extends DefaultDomain {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Size(max = 300)
+    @Column(columnDefinition = "name")
+    private String name;
+
+    @Size(max = 2000)
+    @Column(columnDefinition = "description")
+    private String description;
+
+    @Size(max = 240)
+    @Column(columnDefinition = "author_name")
+    private String authorName;
+
+    @Column(columnDefinition = "author_profile_image_url")
+    private String authorProfileImageUrl;
+
+    @Column(columnDefinition = "likes")
+    private int likes;
+}
---
+++ src/main/java/com/greatideas/demo/persistence/dtos/IdeaDto.java
@@ -0,0 +1,13 @@
+package com.greatideas.demo.persistence.dtos;
+
+import com.greatideas.demo.persistence.domain.DefaultDomain;
+
+public class IdeaDto extends DefaultDomain {
+
+    private Long id;
+    private String name;
+    private String description;
+    private String authorName;
+    private String authorProfileImageUrl;
+    private int likes;
+}
